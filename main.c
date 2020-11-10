da5e7ff4a2d58a7679885fd5ddf2961692d0ffa8
Александр Олешкевич <saugustu@ga-r5.msk.21-school.ru>
2020-11-10 23:10:39 +0300

    added split delimiter case check

--- main.c
+++ main.c
@@ -4,12 +4,30 @@
 int		main(void)
 {
 	printf("checking atoi crutch\n");
-	printf("expected: %d\n", 987);
-	printf("__test__: %d\n", ft_atoi("000000000000000000000987"));
+	printf("expected: %d\n", 42);
+	printf("__test__: %d\n", ft_atoi("000000000000000000000042"));
 	printf("\n");
 
 	printf("checking substr return when index out of string\n");
-	printf("expected_until_|: %s|\n", "");
-	printf("__test___until_|: %s|\n", ft_substr("HelloCat", 15, 3));
+	char *substr_ret;
+	substr_ret = ft_substr("HelloCat", 15, 3);
+	if (substr_ret == NULL)
+		printf("you return NULL :(\n");
+	else
+	{
+		printf("expected_until_|: %s|\n", "");
+		printf("__test___until_|: %s|\n", substr_ret);
+	}
 	printf("\n");
+
+	printf("checking split with a string containing only delimiters\n");
+	char **splited;
+	splited = ft_split("    ", ' ');
+	if (splited == NULL)
+		printf("you return NULL :(\n");
+	else
+	{
+		printf("expected: %s\n", NULL);
+		printf("__test__: %s\n", *splited);
+	}
 }
