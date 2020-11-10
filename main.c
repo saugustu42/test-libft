d8e0cf5794cb31b90e7b7741440f993f056953d7
Александр Олешкевич <saugustu@ga-r5.msk.21-school.ru>
2020-11-10 22:43:11 +0300

    added substr check

--- main.c
+++ main.c
@@ -7,4 +7,9 @@ int		main(void)
 	printf("expected: %d\n", 987);
 	printf("__test__: %d\n", ft_atoi("000000000000000000000987"));
 	printf("\n");
+
+	printf("checking substr return when index out of string\n");
+	printf("expected_until_|: %s|\n", "");
+	printf("__test___until_|: %s|\n", ft_substr("HelloCat", 15, 3));
+	printf("\n");
 }
